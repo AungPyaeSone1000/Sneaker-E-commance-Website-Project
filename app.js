b7fb6cd3043f04075180c371392085a1e8788329
Aung Pyae Sone <ww.aps2002@gmail.com>
2023-08-05 10:28:11 -0400

    Added Sliding animation

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const sliderItems = document.getElementsByClassName('slider-item');
+const sliderWrapper = document.getElementsByClassName("slider-wrapper")[0];
+const sliderItems = document.getElementsByClassName("slider-item");
+const menuItems = document.querySelectorAll(".menu-item");
 
 
-//Assign Random colour to background circle
+//Assign Random colour to background circle and price tag
 for (let i = 0; i < sliderItems.length; i++) {
   const sliderBg = sliderItems[i].getElementsByClassName('slider-bg')[0];
-  const randomColor = getRandomColor();
+  const sliderPrice = sliderItems[i].getElementsByClassName('slider-price')[0];
+  const randomColor = getRandomColour();
   sliderBg.style.backgroundColor = randomColor;
+  sliderPrice.style.color = randomColor;
 }
+
 //Random colour generator
 function getRandomColour() {
     const letters = '0123456789ABCDEF';
@@ -16,3 +21,9 @@ function getRandomColour() {
     }
     return color;
 }
+
+menuItems.forEach((item,index)=>{
+    item.addEventListener("click",()=>{
+        sliderWrapper.style.transform = `translateX(${-100 * index}vw)`;
+    })
+})
